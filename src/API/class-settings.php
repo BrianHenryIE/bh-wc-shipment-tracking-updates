ce6504dc6ceef692685476f4dbefc60b2ec92421
Brian Henry <BrianHenryIE@gmail.com>
2022-09-20 13:32:44 -0700

    2.9.0

--- src/API/class-settings.php
+++ src/API/class-settings.php
@@ -45,7 +45,7 @@ class Settings implements Settings_Interface, WooCommerce_Logger_Settings_Interf
 	 * @return string
 	 */
 	public function get_plugin_version(): string {
-		return defined( 'BH_WC_SHIPMENT_TRACKING_UPDATES_VERSION' ) ? BH_WC_SHIPMENT_TRACKING_UPDATES_VERSION : '2.8.0';
+		return defined( 'BH_WC_SHIPMENT_TRACKING_UPDATES_VERSION' ) ? BH_WC_SHIPMENT_TRACKING_UPDATES_VERSION : '2.9.0';
 	}
 
 	/**
