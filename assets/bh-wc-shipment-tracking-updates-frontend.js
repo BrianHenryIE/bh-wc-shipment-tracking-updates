6f906c9850c9f96dd02ee05ed37308fa2fb99842
Brian Henry <BrianHenryIE@gmail.com>
2022-09-23 13:59:47 -0700

    Add confirmation dialog on my-account 'mark order complete' button

--- assets/bh-wc-shipment-tracking-updates-frontend.js
+++ assets/bh-wc-shipment-tracking-updates-frontend.js
@@ -2,6 +2,11 @@
 	'use strict';
 
 	// Click handler for marking orders paid when on the my-account orders list.
-
+	// Add a confirmation dialog before marking orders completed, since some customer seem to have inadvertently done so.
+	$(function() {
+		$('a.mark-completed').on('click', function () {
+			return confirm('Are you sure you want to mark this order completed?');
+		});
+	});
 
 })( jQuery );
